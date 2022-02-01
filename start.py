9a676b3142b6ddde62d3ce651306718e693375bf
Anubhav Tomar <imanubhav18@gmail.com>
2022-02-02 05:16:53 +0530

    added spotify api tested for playlists

--- start.py
+++ start.py
@@ -1,18 +1,21 @@
-
 import spotipy
+from credentials import my_client_id, my_client_secret
 
 from spotipy.oauth2 import SpotifyClientCredentials
 
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="f82b1ec8efbe4046be621bff39f31ff9",
-                                                           client_secret="89fe075de282419eaff569d56a8af2a6"))
+sp = spotipy.Spotify(
+    auth_manager=SpotifyClientCredentials(
+        client_id=my_client_id, client_secret=my_client_secret
+    )
+)
 
 # results = sp.search(q='atif', limit=20)
 # for idx, track in enumerate(results['tracks']['items']):
-    # print(idx, track['name'])
+# print(idx, track['name'])
 
 playlist = sp.playlist(playlist_id="19Qd1TPko613J71jdkNKnP")
-for u in range(len(playlist)-1):
-    print(playlist['tracks']['items'][u]['track']['name'])
+for u in range(len(playlist) - 1):
+    print(playlist["tracks"]["items"][u]["track"]["name"])
 
 # print(playlist['tracks']['items'][0]['track']['name'])
 # print(playlist['tracks']['items'][0]['track']['album']['name'])
