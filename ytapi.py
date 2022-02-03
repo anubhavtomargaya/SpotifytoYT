8668c4b2b27f1f1b7674d8dc20ca841d1ee30f68
Anubhav Tomar <imanubhav18@gmail.com>
2022-02-03 18:43:06 +0530

    finished addings functions for the whole feature, connections remaining

--- ytapi.py
+++ ytapi.py
@@ -3,21 +3,32 @@ from re import search
 from googleapiclient.discovery import build
 from credentials import yt_api,yt_client_secrent,yt_client_id
 import json
+from spotify_extract import query_builder
+import time
 
 service = build(serviceName='youtube',version='v3',developerKey=yt_api)
 
-request = service.search().list(part="snippet",
-        maxResults=2,
-        q = 'Rishte Naate - From "De Dana Dan" Tera Mera Saath Ho Rahat Fateh Ali Khan'
-    )
 
-response = request.execute()
+queries = ['Tere Mast Mast Do Nain Dabangg Rahat Fateh Ali Khan', 'Rishte Naate - From "De Dana Dan" Tera Mera Saath Ho Rahat Fateh Ali Khan', 'Sanu Ek Pal Chain (From "Raid") Sanu Ek Pal Chain (From "Raid") Rahat Fateh Ali Khan', 'Aas Paas Khuda Anjaana Anjaani Rahat Fateh Ali Khan', 'Dil To Bachcha Hai Ishqiya Rahat Fateh Ali Khan', 'Yeh Jo Halki Halki Khumariya Son Of Sardaar Rahat Fateh Ali Khan', 'Milegi Milegi (From "Stree") Milegi Milegi (From "Stree") Mika Singh', 'Tu Mera Hero Desi Boyz Pritam', 'Jugni Tanu Weds Manu Mika Singh', 'Saawan Mein Lag Gayee Aag Saawan Mein Lag Gayee Aag Mika Singh', 'Jumme Ki Raat Kick Mika Singh', 'Dagabaaz Re Dabangg 2 Rahat Fateh Ali Khan', 'Pungi Agent Vinod Pritam', 'Tu Mere Agal Bagal Hai Phata Poster Nikhla Hero (Original Motion Picture Soundtrack) Mika Singh']
 
-id = response['items'][0]['id']['videoId']
-print(id)
+def getVideoIds(queris):
+    ids = []
+    for i in range(len(queris)):
+        request = service.search().list(part="snippet",
+                maxResults=2,
+                q = queris[i]
+            )
+        response = request.execute()
+        videoid = response['items'][0]['id']['videoId']
+        print(videoid)
 
-req_vdo = service.videos().list(part='snippet', id=id)
-x = req_vdo.execute()
+        ids.append(videoid)
+        time.sleep(10)
+    return ids
+print(getVideoIds(queries))
+# req_vdo = service.videos().list(part='snippet', id=id)
 
-y = json.dumps(x)
-print(y)
+# x = req_vdo.execute()
+# y = json.dumps(x)
+# print(y)
+
